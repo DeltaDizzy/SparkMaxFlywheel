6877ae4488436759d797849e878f8725c1679bbb
DeltaDizzy <EJBraswell@gmail.com>
2022-01-29 10:55:22 -0600

    first commit

---
+++ src/main/java/frc/robot/commands/RunWheelPID.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Flywheel;
+import io.github.oblarg.oblog.annotations.Config;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class RunWheelPID extends CommandBase {
+  private final Flywheel wheel;
+  private int rpmSetpoint;
+  @Config(name = "P")
+  double p = 0.001;
+  @Config(name = "I")
+  double i = 0;
+  @Config(name = "D")
+  double d = 0;
+  @Config(name = "F")
+  double f = 0;
+  double max = 0;
+  double min = -1;
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public RunWheelPID(Flywheel subsystem) {
+    wheel = subsystem;
+    rpmSetpoint = 0;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    wheel.UpdatePID(p, i, d, f, max, min);
+    wheel.setSpeedPID(rpmSetpoint);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) 
+  {
+    wheel.setSpeedPID(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Flywheel.java
@@ -0,0 +1,108 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.oblarg.oblog.annotations.Config;
+
+public class Flywheel extends SubsystemBase 
+{
+  private double gearRatio = 1;
+  private CANSparkMax neo;
+  private TalonSRX cims;
+  private int neoCANId = 14;
+  private int talonCANId = 5;
+  private SparkMaxPIDController pid;
+  private boolean running;
+  double kP, kI, kD, kF, kMaxOutput, kMinOutput;
+  int rpmGoal;
+
+  // ctre sensor units to rpm
+  public double sensorUnitsToRPM(double units) {
+    return (600 * units * gearRatio) / 2048;
+  }
+
+  public  double rpmToSensorUnits(int rpm) {
+    double units = (rpm / 600) * (2048 / gearRatio);
+    return units;
+  }
+
+  /** Creates a new Flywheel. */
+  public Flywheel() 
+  {
+    neo = new CANSparkMax(neoCANId, MotorType.kBrushless);
+    pid = neo.getPIDController();
+
+    cims = new TalonSRX(talonCANId);
+    
+    // default pid gains, adjust in hardware client?
+    kP = 0.0001;
+    kI = 0;
+    kD = 0;
+    kF = 0;
+    kMinOutput = -1;
+    kMaxOutput = 0;
+    rpmGoal = 0;
+
+    pid.setP(kP);
+    pid.setI(kI);
+    pid.setD(kD);
+    pid.setFF(kF);
+    pid.setOutputRange(kMinOutput, kMaxOutput);
+
+    neo.setIdleMode(IdleMode.kCoast);
+    neo.setOpenLoopRampRate(1.5);
+    neo.setClosedLoopRampRate(1.5);
+  }
+
+  public void UpdatePID(double p, double i, double d, double f, double max, double min)
+  {
+    if((p != kP)) { pid.setP(p); kP = p; }
+    if((i != kI)) { pid.setI(i); kI = i; }
+    if((d != kD)) { pid.setD(d); kD = d; }
+    if((f != kF)) { pid.setFF(f); kF = f; }
+    if((max != kMaxOutput) || (min != kMinOutput)) 
+    { 
+      pid.setOutputRange(min, max); 
+      kMinOutput = min; kMaxOutput = max; 
+    }
+  }
+
+  public void setSpeedPID(int setpoint)
+  {
+    if (!running) {
+      setpoint = 0;
+    }
+    rpmGoal = setpoint;
+    setMotor(rpmGoal);
+  }
+
+  @Config
+  private void setMotor(int demand)
+  {
+    pid.setReference(-demand, ControlType.kVelocity);
+    if (demand < 0.05) {
+      cims.set(ControlMode.PercentOutput, 0);
+    }
+    else {
+      cims.set(ControlMode.PercentOutput, 1);
+    }
+    
+  }
+
+  @Config.ToggleButton
+  public void setRunning(boolean run) {
+    running = run;
+
+  }
+}
